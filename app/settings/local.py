44c8993d2fe0ac7c42c1e74929fd8ebdd992b03e
JIYEON HWANG <ghkdwldus0807@gmail.com>
2025-07-24 11:20:10 +0900

    feat: 환경 변수 세팅 파일 추가

---
+++ app/settings/local.py
@@ -0,0 +1,20 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pathlib import Path
+
+
+class LocalSettings(BaseSettings):
+    DB_HOST: str
+    DB_PORT: int
+    DB_USERNAME: str
+    DB_PASSWORD: str
+    DB_NAME: str
+    W2V_MODEL_PATH: str
+    MONGO_URL: str
+
+    model_config = SettingsConfigDict(
+        env_file=str(Path(__file__).resolve().parent.parent.parent / ".env"),
+        env_file_encoding="utf-8",
+    )
+
+
+settings = LocalSettings()
