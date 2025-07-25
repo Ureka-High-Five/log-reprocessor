7e3a698d7d4d0570f7d86c1da2e576e70535a1c3
likerhythm <wjdalsdl0412@gmail.com>
2025-07-25 14:36:27 +0900

    fix[#12]: postgres 의존성 제거

--- app/router/scheduler_router.py
+++ app/router/scheduler_router.py
@@ -9,13 +9,11 @@ router = APIRouter()
 
 @router.post("/resize-weight")
 async def trigger_resize_weight(request: Request):
-    pg_pool = request.app.state.pg_pool
     mongo_client = request.app.state.mongo_client
 
     action_log_repo = ActionLogRepository(mongo_client)
     user_weight_repo = UserWeightRepository(mongo_client)
-    get_genres_func = partial(get_genres_by_content_id, pg_pool)
 
-    await resize_weight(action_log_repo, user_weight_repo, get_genres_func)
+    await resize_weight(action_log_repo, user_weight_repo)
 
     return {"message": "ok"}
