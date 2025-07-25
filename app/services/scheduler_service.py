6b20837d316acc8b601aa3cf352e733d03b4d117
likerhythm <wjdalsdl0412@gmail.com>
2025-07-25 14:36:47 +0900

    feat[#12]: 재시도 로직 작성

--- app/services/scheduler_service.py
+++ app/services/scheduler_service.py
@@ -1,3 +1,5 @@
+import asyncio
+import logging
 import math
 import time
 import numpy as np
@@ -12,15 +14,32 @@ from app.util.weight_aging import exponential_decay_weight
 from app.repositories.action_log_repository import ActionLogRepository
 from app.repositories.user_weight_repository import UserWeightRepository
 
+MAX_RETRIES = 3
+RETRY_DELAY_SEC = 1
+
 async def resize_weight(
     action_log_repo: ActionLogRepository,
     user_weight_repo: UserWeightRepository,
 ):
-    # 모든 로그 조회
-    all_logs = await action_log_repo.find_all_order_by_user_id()
+    # ✅ 모든 로그 조회 재시도 로직
+    all_logs = None
+    for attempt in range(1, MAX_RETRIES + 1):
+        try:
+            all_logs = await action_log_repo.find_all_order_by_user_id()
+            break
+        except Exception as e:
+            logging.warning(f"[daily_weight_resizer][{attempt}/{MAX_RETRIES}] 로그 조회 실패: {e}")
+            if attempt < MAX_RETRIES:
+                await asyncio.sleep(RETRY_DELAY_SEC)
+            else:
+                logging.error(f"[daily_weight_resizer] 모든 재시도 실패, 로그 조회 불가, error: {e}")
+                return
+
+
     # 사용자별로 로그 분리
     grouped_logs = group_logs_by_user_id(all_logs)
 
+    failed = []
     for user_id, logs in grouped_logs.items():
         genre_dict = defaultdict(int)
         for log in logs:
@@ -41,8 +60,11 @@ async def resize_weight(
 
         # MongoDB에 resized 가중치 저장
         for genre_name, resized_weight in genre_dict.items():
-            await user_weight_repo.reset_weight(user_id, genre_name, resized_weight)
-        
+            try:
+                await user_weight_repo.reset_weight(user_id, genre_name, resized_weight)
+            except:
+                failed.append((user_id, genre_name, resize_weight))
+                
         # resized 가중치 기반으로 벡터 계산
         vector = calc_user_vector(genre_dict)
         vector_str = np.array2string(vector, separator=', ')
@@ -53,6 +75,18 @@ async def resize_weight(
         except Exception as e:
             print(f'❌ 사용자 {user_id} 벡터 Redis 저장 실패: {e}')
 
+        # 실패 로그 재시도
+        for user_id, genre_name, resize_weight in failed:
+            for attempt in range(1, MAX_RETRIES + 1):
+                try:
+                    await user_weight_repo.reset_weight(user_id, genre_name, resized_weight)
+                except Exception as e:
+                    if (attempt < MAX_RETRIES):
+                        logging.warning(f"[daily_weight_resizer][{attempt}/{MAX_RETRIES}] 보상 트랜잭션 재시도")
+                    else:
+                        logging.error(
+                            f"[daily_weight_resizer] 보상트랜잭션 실패, log_id: {log['_id']}, error: {e}"
+                        )
     print("✅ 가중치 resizing 완료")
     return
 
