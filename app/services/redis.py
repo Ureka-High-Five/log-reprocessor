b02dccedb3d718bcc1512b55475e00eb5412ea1d
JIYEON HWANG <ghkdwldus0807@gmail.com>
2025-07-30 09:24:39 +0900

    fix[#19] : redis 설정 추가

--- app/services/redis.py
+++ app/services/redis.py
@@ -3,9 +3,21 @@ from app.settings import settings
 
 redis = None
 
+
+def get_redis() -> Redis:
+    if redis is None:
+        raise RuntimeError("Redis가 초기화되지 않았습니다.")
+    return redis
+
 async def init_redis():
     global redis
-    redis = Redis(host=settings.DEV_REDIS_HOST, port=settings.DEV_REDIS_PORT, decode_responses=True)
+    redis = Redis(host=settings.DEV_REDIS_HOST,
+                  port=settings.DEV_REDIS_PORT,
+                  decode_responses=True, 
+                  socket_timeout=5,
+                  socket_connect_timeout=5,
+                  max_connections=50,
+                  ssl=True,)
     print(f"✅ Redis 연결 성공: {redis}")
 
 async def close_redis():
@@ -14,4 +26,7 @@ async def close_redis():
         print("🧹 Redis 연결 종료")
 
 async def save_user_vector(user_id: int, value: str):
-    await redis.set(user_id, value)
+    try:
+        await get_redis().set(str(user_id), value)
+    except Exception as e:
+        print(f"❌ Redis 저장 실패: {e}")
