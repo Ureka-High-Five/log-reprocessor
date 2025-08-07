ae1087995e6ed6b82cea48e0461cc32cf96c9c92
sangyunpark99 <psangyun5108@gmail.com>
2025-08-07 10:10:42 +0900

    feat: 장르 추가, 모든 장르 한글로 변경

--- app/models/db_w2v_mapper.py
+++ app/models/db_w2v_mapper.py
@@ -16,11 +16,8 @@ _genre_mapping = {
     "역사": "History",
     "음악": "Music",
     "전쟁": "War",
-    "Action & Adventure": "Adventure",
-    "Sci-Fi & Fantasy": "Fantasy",
-    "Kids": "Children",
-    "War & Politics": "War",
-    "Talk": "Talkshow",
+    "토크쇼": "Talkshow",
+    "리얼리티": "Reality"
 }
 
 
